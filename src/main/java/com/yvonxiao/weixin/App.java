d71c188169d443508026b805864371a3eddd0804
Yvon Xiao <yvonxiao@gmail.com>
2015-01-11 20:53:32 +0800

    初始化源代码文件夹

---
+++ src/main/java/com/yvonxiao/weixin/App.java
@@ -0,0 +1,13 @@
+package com.yvonxiao.weixin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
